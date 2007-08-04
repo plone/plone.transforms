6a400c5e0b939419e641b11d40b74437cb6f4d5e
Hanno Schlichting <hanno@hannosch.eu>
2007-08-04 09:12:57 +0000

    Added HtmlToTextTransform written by tbenita.
    svn path=/plone.transforms/trunk/; revision=16302

---
+++ plone/transforms/text/html_text.py
@@ -0,0 +1,47 @@
+"""
+HTML to Text
+"""
+from zope.interface import implements
+
+from plone.transforms.interfaces import ITransform
+from plone.transforms.message import PloneMessageFactory as _
+from plone.transforms.transform import Transform
+from plone.transforms.transform import TransformResult
+
+import re
+
+class HtmlToTextTransform(Transform):
+    """A transform which transforms HTML into Text.
+
+    Let's make sure that this implementation actually fulfills the API.
+
+      >>> from zope.interface.verify import verifyClass
+      >>> verifyClass(ITransform, HtmlToTextTransform)
+      True
+    """
+
+    implements(ITransform)
+
+    name = u'plone.transforms.text.html_text.HtmlToTextTransform'
+
+    title = _(u'title_html_to_text_transform',
+        default=u'HTML to Text transform')
+
+    description = _(u'description_html_to_text_transform',
+        default=u"A transform which transforms HTML into Text.")
+
+    inputs  = ("text/html",)
+    output = "text/plain"
+
+    def transform(self, data):
+        # TODO convert entites with htmlentitydefs.name2codepoint ?
+        for convertion_chain in ( 
+                       '<script [^>]>.*</script>(?im)',
+                       '<style [^>]>.*</style>(?im)',
+                       '<head [^>]>.*</head>(?im)',
+                       '(?im)</?(font|em|i|strong|b)(?=\W)[^>]*>',
+                       '<[^>]*>(?i)(?m)',
+                       ) :
+            r = re.compile( convertion_chain )
+            data = r.sub(' ', u''.join(data))
+        return TransformResult(iter(data))
---
+++ plone/transforms/text/tests/test_html_text.py
@@ -0,0 +1,63 @@
+# -*- coding: UTF-8 -*-
+"""
+    Tests for the HTML to text transform.
+"""
+
+import unittest
+
+from zope.component import queryUtility
+from zope.component.testing import tearDown
+from zope.testing import doctest
+from zope.testing.doctestunit import DocTestSuite
+
+from plone.transforms.interfaces import ITransform
+
+from plone.transforms.tests.utils import configurationSetUp
+
+
+def testHtmlTextTransform():
+    r"""
+    First we get the transform utility.
+
+      >>> util = queryUtility(ITransform,
+      ...            name='plone.transforms.text.html_text.HtmlToTextTransform')
+      >>> util
+      <plone.transforms.text.html_text.HtmlToTextTransform object at ...>
+
+    Set up some test text:
+
+      >>> text = unicode("\n<html><head><title>Stupid title</title></head>"
+      ...                "<p>Some simple test text héhé.\n</p>\n\n\n</html>", 'utf-8')
+      >>> data = iter(text)
+
+    Now transform the data:
+
+      >>> result = util.transform(data)
+
+    And check the result:
+
+      >>> result
+      <plone.transforms.transform.TransformResult object at ...>
+
+      >>> result.data
+      <iterator object at ...>
+
+      >>> text = u''.join(result.data)
+      >>> text.strip()
+      u'Stupid title   Some simple test text h\xe9h\xe9.'
+    """
+
+
+def test_suite():
+    suite = unittest.TestSuite((
+        DocTestSuite('plone.transforms.text.html_text'),
+        ))
+    suite.addTest(
+        DocTestSuite(setUp=configurationSetUp,
+                     tearDown=tearDown,
+                     optionflags=doctest.ELLIPSIS | 
+                        doctest.NORMALIZE_WHITESPACE))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest="test_suite")
