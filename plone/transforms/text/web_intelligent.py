6bd69ea7569ef2ae71ca4d944eb60513ce3f9c6b
Hanno Schlichting <hanno@hannosch.eu>
2007-09-05 00:04:14 +0000

    Added plone.intelligenttext transforms
    svn path=/plone.transforms/trunk/; revision=16917

---
+++ plone/transforms/text/web_intelligent.py
@@ -0,0 +1,76 @@
+"""
+
+"""
+from zope.interface import implements
+
+from plone.transforms.interfaces import ITransform
+from plone.transforms.message import PloneMessageFactory as _
+from plone.transforms.stringiter import StringIter
+from plone.transforms.transform import Transform
+from plone.transforms.transform import TransformResult
+
+HAS_WEB_INTELLIGENTTEXT = True
+try:
+    from plone.intelligenttext.transforms import (
+        convertWebIntelligentPlainTextToHtml,
+        convertHtmlToWebIntelligentPlainText)
+except ImportError:
+    HAS_WEB_INTELLIGENTTEXT = False
+
+
+class WebIntelligentHtmlTransform(Transform):
+    """A transform which transforms web intelligent text into HTML."""
+
+    implements(ITransform)
+
+    name = u'plone.transforms.text.web_intelligent.WebIntelligentHtmlTransform'
+
+    title = _(u'title_web_intelligent_html_transform',
+        default=u'Web Intelligent Text to HTML transform')
+
+    inputs  = ("text/x-web-intelligent",)
+    output = "text/html"
+
+    available = False
+
+    def __init__(self):
+        super(WebIntelligentHtmlTransform, self).__init__()
+        if HAS_WEB_INTELLIGENTTEXT:
+            self.available = True
+
+    def transform(self, data):
+        """Returns the transform result."""
+        if self._validate(data) is None:
+            return None
+
+        html = convertWebIntelligentPlainTextToHtml(u''.join(data))
+        return TransformResult(StringIter(unicode(html, 'utf-8')))
+
+
+class HtmlWebIntelligentTransform(Transform):
+    """A transform which transforms HTML into web intelligent text."""
+
+    implements(ITransform)
+
+    name = u'plone.transforms.text.web_intelligent.HtmlWebIntelligentTransform'
+
+    title = _(u'title_html_web_intelligent_transform',
+        default=u'HTML to Web Intelligent Text transform')
+
+    inputs  = ("text/html",)
+    output = "text/x-web-intelligent"
+
+    available = False
+
+    def __init__(self):
+        super(HtmlWebIntelligentTransform, self).__init__()
+        if HAS_WEB_INTELLIGENTTEXT:
+            self.available = True
+
+    def transform(self, data):
+        """Returns the transform result."""
+        if self._validate(data) is None:
+            return None
+
+        html = convertHtmlToWebIntelligentPlainText(u''.join(data))
+        return TransformResult(StringIter(html))
